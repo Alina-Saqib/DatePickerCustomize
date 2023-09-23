171768eda11928671064474b2f652cd08f31e5b9
Alina-Saqib <alina.saqib500@gmail.com>
2023-09-23 14:53:15 +0500

    Changes

--- src/components/CustomDatePicker.tsx
+++ src/components/CustomDatePicker.tsx
@@ -24,7 +24,6 @@ const CustomDatePicker: React.FC<CustomDatePickerProps> = ({value,onChange,disab
       const formatSelectedDate = (date: Date) => {
      console.log(date);
         if (!date || isNaN(date.getTime())) return "";
-        // Get the components of the date
        const year = date.getFullYear().toString().padStart(4, '0');
        const month = (date.getMonth() + 1).toString().padStart(2, '0');
        const day = date.getDate().toString().padStart(2, '0');
@@ -53,19 +52,16 @@ const CustomDatePicker: React.FC<CustomDatePickerProps> = ({value,onChange,disab
 
       const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const inputValue = e.target.value;
-        const dateRegex = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}$/;
+        setInputValue(inputValue);
+      };
 
-        if (dateRegex.test(inputValue)) {
-          // The entered date matches the expected format, format and update the input value
+      const handleInputblur = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const inputValue = e.target.value;
           const date = new Date(inputValue);
           const formattedDate = formatSelectedDate(date);
           setInputValue(formattedDate);
           onChange(formattedDate);
-        } else {
-          // The entered date does not match the expected format, you can handle this case here (e.g., show an error message)
-          // For now, I'm just updating the input value to the entered text
-          setInputValue(inputValue);
-        }
+        
       };
 
  const handleIncrementHour = () => {
@@ -105,6 +101,7 @@ const CustomDatePicker: React.FC<CustomDatePickerProps> = ({value,onChange,disab
         placeholder="YYYY-MM-DDTHH:mm:SS"
         suffix={<CalendarOutlined onClick={toggleCalendar} />}
         onChange={(e) => handleInputChange(e)}
+        onBlur={ (e) => handleInputblur(e)}
         onKeyUp={handleKeyPress}
         size='large'
         style={{ width: '300px' }} 
